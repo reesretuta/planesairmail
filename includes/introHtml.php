1ba52f78498f6593fa5b5ad315d529135e661d1e
Alec McCormick <amccormick@bltomato.com>
2014-06-20 18:48:23 -0700

    begin screen animations + intro animations

--- includes/introHtml.php
+++ includes/introHtml.php
@@ -9,15 +9,17 @@
             <div class="background-image">
                 <div class="empty-space"></div>
             </div>
-
-
         </div>
         <div class="viewport bottom">
             <div class="background-image">
                 <div class="empty-space"></div>
             </div>
-
-
+        </div>
+        <div class="begin-screen">
+            <div class="line">Send a message to your favorite member</div>
+            <div class="line">of the piston peak air attack team and</div>
+            <div class="line">they'll send you back some airmail!</div>
+            <a href="#" class="begin">Begin</a>
         </div>
     </div>
 </div>
--- js/src/views/introView.js
+++ js/src/views/introView.js
@@ -8,7 +8,9 @@
 
     var IntroView = Backbone.View.extend({
         el: '#intro-view',
-
+        events: {
+            'click a.begin': 'onBeginClick'
+        },
         // ============================================================ //
         /* ****************** Initialization Stuff ******************** */
         // ============================================================ //
@@ -26,28 +28,77 @@
 
             this.$viewPortTop = $viewPorts.filter('.top');
             this.$viewPortBottom = $viewPorts.filter('.bottom');
+
+            this.$beginScreen = this.$el.find('div.begin-screen');
+            this.$beginLines = this.$beginScreen.find('div.line');
+            this.$beginBtn = this.$beginScreen.find('a.begin');
         },
         initAnimationTimeline: function() {
-            this.timeline = this.getTimelineHide();
+            this.timelineHide = this.getTimelineHide();
+            this.timelineBeginScreenIn = this.getTimelineBeginScreenIn();
         },
         initScene: function() {
             this.scene = scenesManager.createScene('intro', IntroScene);
 
-            this.scene.onComplete(_.bind(this.hide, this));
+            this.scene.onComplete(_.bind(this.showBeginScreen, this));
+//            this.scene.onComplete(_.bind(this.hide, this));
         },
 
         // ============================================================ //
         /* ******************** Render Functions ********************** */
         // ============================================================ //
-        render: function() {
+        showBeginScreen: function() {
+            var timeline = this.timelineBeginScreenIn;
 
-
-            return this;
+            setTimeout(_.bind(timeline.play, timeline), 200);
         },
+
 
         // ============================================================ //
         /* ***************** Animation Functions ********************** */
         // ============================================================ //
+        getTimelineBeginScreenIn: function() {
+            /****************** Static Variables **************/
+            var animationTime = 0.5;
+            var easing = 'Back.easeOut';
+
+
+            var tweens = _.map(this.$beginLines, function(line) {
+                return TweenLite.to(line, animationTime, {
+                    x: 0,
+                    opacity: 1,
+                    ease: easing
+                });
+            });
+
+            var timeline = new TimelineMax({
+                paused: true,
+                tweens: tweens,
+                stagger: 0.1,
+                onStart: function() {
+                    TweenLite.set(this.$beginBtn, {
+                        scale: 0.7
+                    });
+                },
+                onStartScope: this
+            });
+
+
+            var btnInTime = 0.5;
+
+            timeline.add(TweenLite.to(this.$beginBtn, animationTime, {
+                opacity: 1,
+                scaleY: 1,
+                ease: 'Elastic.easeOut'
+            }), btnInTime);
+            timeline.add(TweenLite.to(this.$beginBtn, animationTime, {
+                scaleX: 1,
+                ease: 'Elastic.easeOut'
+            }), btnInTime + (animationTime * 0.05));
+
+
+            return timeline;
+        },
 
         getTimelineHide: function() {
             /****************** Static Variables **************/
@@ -60,6 +111,11 @@
                 onComplete: this.onAnimationFinished,
                 onCompleteScope: this
             });
+
+            timeline.add(TweenLite.to(this.$beginScreen, animationTime/4, {
+                opacity: 0,
+                ease: easing
+            }), 0);
 
             timeline.add(TweenLite.to(this.$viewPortTop, animationTime, {
                 height: 0,
@@ -86,6 +142,16 @@
         },
 
         // ============================================================ //
+        /* ************************* Events *************************** */
+        // ============================================================ //
+
+        onBeginClick: function(e) {
+            e.preventDefault();
+
+            this.hide();
+        },
+
+        // ============================================================ //
         /* *********************** Public API ************************* */
         // ============================================================ //
         start: function() {
@@ -96,11 +162,12 @@
             this.scene.startAnimation();
         },
         hide: function() {
-            this.timeline.play();
+            this.timelineHide.play();
         },
         onComplete: function(callback) {
             this.onCompleteCallback = callback;
         }
+
     });
 
 
