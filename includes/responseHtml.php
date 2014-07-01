2f706649d729a7131c7be4209f453e7884f7a176
Rees Retuta <rees.retuta@gmail.com>
2014-06-30 19:47:01 -0700

    rees - results page updates

--- includes/responseHtml.php
+++ includes/responseHtml.php
@@ -3,6 +3,19 @@
 
 <div id="response" class="full-absolute">
     <div id="card-wrap">
-        
+        <div id="card-header">
+            Dear Michael,
+        </div>
+        <div id="card-body">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div id="card-sincerely">
+            thanks for writing to mus
+        </div>
+        <div id="card-from">
+            michael
+        </div>
     </div>
+    <a id="name">Send More Airmail</a>
 </div>
